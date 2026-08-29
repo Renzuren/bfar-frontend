b95ef5c71fc5e3cc24e5d01111831401c1cfb25c
Deyi <109661279+DreiDrei0520@users.noreply.github.com>
2026-08-30 02:41:54 +0800

    Update NonBeneficiaryTab.js

--- src/pages/NonBeneficiaryTab.js
+++ src/pages/NonBeneficiaryTab.js
@@ -28,6 +28,7 @@ import {
 import { toast } from 'sonner';
 import { api } from '../lib/apiMiddleware';
 import { useProject } from '../context/ProjectContext';
+import { copyToClipboard } from '../lib/utils';
 
 const NonBeneficiaryTab = () => {
   const outletCtx = useOutletContext();
@@ -62,11 +63,15 @@ const NonBeneficiaryTab = () => {
     fetchData();
   }, [project]);
 
-  const copyFormLink = () => {
+  const copyFormLink = async () => {
     if (!project?.after_form) return;
     const link = `${window.location.origin}/f/${project.after_form}`;
-    navigator.clipboard.writeText(link);
-    toast.success('Questionnaire link copied!');
+    const success = await copyToClipboard(link);
+    if (success) {
+      toast.success('Questionnaire link copied!');
+    } else {
+      toast.error('Could not copy the link. Please copy it manually.');
+    }
   };
 
   const copyFromBeneficiary = async () => {
